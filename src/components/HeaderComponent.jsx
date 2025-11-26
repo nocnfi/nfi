bea3d3a35015be3ef5349b638aaaeeb442b2b3ba
nocnfi-arch <nocnfi@gmail.com>
2025-11-26 16:12:08 +0700

    change headbar style

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -6,14 +6,16 @@ import hamMenuClose from '../assets/ham-menu-close.svg'
 const HeaderComponent = () => {
   return (
     <>
-        <header className='flex px-4 sm:px-8 py-4 justify-between fixed w-full bg-white'>
-            <div><img className='w-48 sm:w-64' src={nfilogo} alt="nfilogo" /></div>
-            <nav className='sm:flex hidden'>
-              <button className='w-20 cursor-pointer text-blue-400 font-bold'>BERANDA</button>
-              <button className='w-20 cursor-pointer hover:font-bold'>PROFIL</button>
-              <button className='w-20 cursor-pointer hover:font-bold'>LAYANAN</button>
-              <button className='w-20 cursor-pointer hover:font-bold'>KONTAK</button>
-            </nav>
+        <header className='z-50 flex my-4 mx-12 fixed w-full'>
+            <div className='backdrop-blur-md rounded-xl p-2 bg-white/60'><img className='w-40' src={nfilogo} alt="nfilogo" /></div>
+            <div className='flex justify-center w-full'>
+              <nav className='flex backdrop-blur-md rounded-full p-2 bg-white/60 text-sm text-gray-600'>
+                <button className='w-30 rounded-full p-2 bg-white/50 cursor-pointer font-bold'>BERANDA</button>
+                <button className='w-30 cursor-pointer hover:font-bold'>PROFIL</button>
+                <button className='w-30 cursor-pointer hover:font-bold'>LAYANAN</button>
+                <button className='w-30 cursor-pointer hover:font-bold'>KONTAK</button>
+              </nav>
+            </div>
             <button className='sm:hidden cursor-pointer'><img className='w-8' src={hamMenu} alt="" /></button>
         </header>
     </>
